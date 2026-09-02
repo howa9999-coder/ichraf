7b50ecd88b01fd997dd53c10b8ea147e44e407a7
Wahiba <73328123+howa9999-coder@users.noreply.github.com>
2026-09-02 23:50:04 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /* ===============================
    CACHE NAME (change version to force update)
    =============================== */
-const cacheName = 'ichraf-v0009';
+const cacheName = 'ichraf-v00010';
 
 /* ===============================
    FILES TO CACHE FOR OFFLINE USE
